baf6c793b845ba088839560283ea1dd3639fc1b4
Sravya-1818 <sravyalakshmitulasivalluri@gmail.com>
2025-06-14 11:44:13 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,22 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import QRGenerator from "./pages/QRGenerator";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import UserProfile from "./pages/UserProfile";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/generate" element={<QRGenerator />} />
+        <Route path="/user/:id" element={<UserProfile />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
