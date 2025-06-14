2d1ca39dc98416670e73803248028a425498c051
Sravya-1818 <sravyalakshmitulasivalluri@gmail.com>
2025-06-14 09:55:58 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { supabase } from "../supabase"; // adjust path if needed
+import { supabase } from "src/lib/supasbase.ts"; // adjust path if needed
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
