6888946067df7096498fb01d8f7f592ae910c79b
Sravya-1818 <sravyalakshmitulasivalluri@gmail.com>
2025-06-14 10:33:23 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,74 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+
 import Header from "./components/Header";
 import ProtectedRoute from "./components/ProtectedRoute";
+
 import Index from "./pages/Index";
 import QRGenerator from "./pages/QRGenerator";
 import UserProfile from "./pages/UserProfile";
 import Login from "./pages/Login";
-
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
 const AppContent = () => {
   const location = useLocation();
-  const hideHeader = location.pathname === '/login' || location.pathname === '/signup';
+  const hideHeader = location.pathname === "/login" || location.pathname === "/signup";
 
   return (
     <div className="min-h-screen">
       {!hideHeader && <Header />}
+
       <Routes>
         <Route path="/login" element={<Login />} />
-       
-        <Route path="/" element={
-          <ProtectedRoute>
-            <Index />
-          </ProtectedRoute>
-        } />
-        <Route path="/generate" element={
-          <ProtectedRoute>
-            <QRGenerator />
-          </ProtectedRoute>
-        } />
-        <Route path="/user/:userId" element={<UserProfile />} />
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Index />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/generate"
+          element={
+            <ProtectedRoute>
+              <QRGenerator />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/user/:userId"
+          element={
+            <ProtectedRoute>
+              <UserProfile />
+            </ProtectedRoute>
+          }
+        />
+
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
 };
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <AppContent />
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <AppContent />
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
