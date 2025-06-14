2fa920746c741cc5c8cf8022b7ef2d166cdcbdfb
Sravya-1818 <sravyalakshmitulasivalluri@gmail.com>
2025-06-14 11:51:43 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,21 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import QRGenerator from "./pages/QRGenerator";
+import UserProfile from "./pages/UserProfile";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
-import UserProfile from "./pages/UserProfile";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/generate" element={<QRGenerator />} />
         <Route path="/user/:id" element={<UserProfile />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
